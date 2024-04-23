70be34d109e011b03a7e6d24cc7bfbec2ed0fa3b
Miguel Beltran <m@beltran.work>
2024-04-23 11:04:32 +0200

    refactor: use equal instead of ok

--- test/raygun_batch_lifecycle_test.js
+++ test/raygun_batch_lifecycle_test.js
@@ -12,7 +12,7 @@ test("batch transport doesn't keep processes alive", async (t) => {
           return reject(error);
         }
 
-        t.ok(process.exitCode === 0);
+        t.equal(process.exitCode, 0);
         resolve();
       }
     );
