95bb3d6d0875469ab5e38c8511165d4b2fd7cb9e
Miguel Beltran <m@beltran.work>
2024-04-24 08:07:30 +0200

    format eslint config file

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,16 +1,20 @@
 import globals from 'globals'
 
 import path from 'path'
-import { fileURLToPath } from 'url'
-import { FlatCompat } from '@eslint/eslintrc'
+import {fileURLToPath} from 'url'
+import {FlatCompat} from '@eslint/eslintrc'
 import pluginJs from '@eslint/js'
 
 // mimic CommonJS variables -- not needed if using CommonJS
 const __filename = fileURLToPath(import.meta.url)
 const __dirname = path.dirname(__filename)
-const compat = new FlatCompat({ baseDirectory: __dirname, recommendedConfig: pluginJs.configs.recommended })
+const compat = new FlatCompat({baseDirectory: __dirname, recommendedConfig: pluginJs.configs.recommended})
 
 export default [
-  { languageOptions: { globals: globals.node } },
-  ...compat.extends('standard-with-typescript')
+    {
+        languageOptions: {
+            globals: globals.node
+        }
+    },
+    ...compat.extends('standard-with-typescript')
 ]
