33401d15a73e76bb7ce0df0a9d2b6cc6e99b32b8
Miguel Beltran <m@beltran.work>
2024-04-26 11:18:40 +0200

    setup no-unused-vars lint rule

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -8,8 +8,20 @@ export default tseslint.config(
     ...tseslint.configs.recommended,
     {
         rules: {
+            "@typescript-eslint/no-unused-vars": [
+                "error",
+                {
+                    // Do not report unused function arguments
+                    "args": "none",
+                    // "argsIgnorePattern": "^_",
+                    // "caughtErrors": "all",
+                    // "caughtErrorsIgnorePattern": "^_",
+                    // "destructuredArrayIgnorePattern": "^_",
+                    // "varsIgnorePattern": "^_",
+                    // "ignoreRestSiblings": true
+                }
+            ],
             // TODO: Remove ignored rules and fix the code
-            "@typescript-eslint/no-unused-vars": "off",
             "@typescript-eslint/no-var-requires": "off",
             "no-undef": "off",
             "no-unreachable": "off",
