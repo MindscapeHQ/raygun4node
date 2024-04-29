1650c25a63f7c0c9e9c625051ae4c6ddc63fc03b
Miguel Beltran <m@beltran.work>
2024-04-29 07:32:25 +0200

    ci: #145 eslint setup (#156)
    * removed tslint
    
    * add eslint
    
    * add default config
    
    * setup default config and npm run command
    
    * add command to node.js.yml
    
    * set rules to support current codebase
    
    * remove unused tslint.json
    
    * include sample files in eslint command

---
+++ eslint.config.mjs
@@ -0,0 +1,21 @@
+// @ts-check
+
+import eslint from '@eslint/js';
+import tseslint from 'typescript-eslint';
+
+export default tseslint.config(
+    eslint.configs.recommended,
+    ...tseslint.configs.recommended,
+    {
+        rules: {
+            // TODO: Remove ignored rules and fix the code
+            "@typescript-eslint/no-explicit-any": "off",
+            "@typescript-eslint/no-this-alias": "off",
+            "@typescript-eslint/no-unused-vars": "off",
+            "@typescript-eslint/no-var-requires": "off",
+            "no-undef": "off",
+            "no-unreachable": "off",
+            "no-useless-catch": "off",
+        }
+    }
+);
