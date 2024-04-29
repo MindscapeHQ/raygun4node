8f4bf1f3cbd3972b1345a97903800cc175001fef
Miguel Beltran <m@beltran.work>
2024-04-29 07:33:05 +0200

    chore: #148 upgrade examples (#157)
    * express example: upgrade dependencies and point to subfolder raygun package
    
    * using-domains: upgrade dependencies and reference local raygun
    
    * using-domains: upgrade dependencies and reference local raygun
    
    * add console logs and format using spaces
    
    * improved readme
    
    * improved readme + replace tabs by spaces
    
    * enforce changing API keys to run examples

--- examples/using-domains/app.js
+++ examples/using-domains/app.js
@@ -1,5 +1,10 @@
 var config = require('config');
 
+if (config.Raygun.Key === 'YOUR_API_KEY') {
+    console.error('You need to set your Raygun API key in the config file');
+    process.exit(1);
+}
+
 // Setup Raygun
 var raygun = require('raygun');
 var raygunClient = new raygun.Client().init({apiKey: config.Raygun.Key});
@@ -11,33 +16,37 @@ var appDomain = require('domain').create();
 // crashes
 appDomain.on('error', function (err) {
     try {
-    	// Try send data to Raygun
+        console.log(`Domain error caught: ${err}`);
+        // Try send data to Raygun
         raygunClient.send(err, {}, function () {
-        	// Exit the process once the error has been sent
+            // Exit the process once the error has been sent
+            console.log('Error sent to Raygun, exiting process');
             process.exit(1);
         });
     } catch (e) {
-		// If there was an error sending to Raygun, log it out and end the process.
-		// Could possibly log out to a text file here
-		console.log(e);
+        // If there was an error sending to Raygun, log it out and end the process.
+        // Could possibly log out to a text file here
+        console.log(e);
         process.exit(1);
     }
 });
 
 // Run the domain
 appDomain.run(function () {
-	var fs = require('fs');
+    var fs = require('fs');
 
-	// Try and read a file that doesn't exist
-	fs.readFile('badfile.json', 'utf8', function(err, file){
-		if(err) {
-			// We could send the error straight to Raygun
-			// raygunClient.send(err);
+    console.log('Running example app');
 
-			// Or we can deal with it in our "Fake Error Handler" below
-			
-			// This will throw an error as fakeErrorHandler doesn't exist
-			fakeErrorHandler.DealWith(err);
-		}
-	})
+    // Try and read a file that doesn't exist
+    fs.readFile('badfile.json', 'utf8', function (err, file) {
+        if (err) {
+            // We could send the error straight to Raygun
+            // raygunClient.send(err);
+
+            // Or we can deal with it in our "Fake Error Handler" below
+
+            // This will throw an error as fakeErrorHandler doesn't exist
+            fakeErrorHandler.DealWith(err);
+        }
+    })
 });
