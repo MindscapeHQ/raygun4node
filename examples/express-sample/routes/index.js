ce51d28379c5d1d286cb1943914755a46a9c0ea1
Miguel Beltran <m@beltran.work>
2024-05-01 07:37:19 +0200

    add missing files

--- examples/express-sample/routes/index.js
+++ examples/express-sample/routes/index.js
@@ -1,17 +1,17 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-	res.render('index', {
-		title: 'Express'
-	});
+router.get("/", function (req, res, next) {
+  res.render("index", {
+    title: "Express",
+  });
 });
 
-router.get('/error', function(req, res, next) {
-	// Call an object that doesn't exist to send an error to Raygun
-	fakeObject.FakeMethod();
-	res.send(500);
+router.get("/error", function (req, res, next) {
+  // Call an object that doesn't exist to send an error to Raygun
+  fakeObject.FakeMethod();
+  res.send(500);
 });
 
 module.exports = router;
