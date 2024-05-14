a92b5865ca629e9283f61ff99116d051c420d4cf
Miguel Beltran <m@beltran.work>
2024-05-14 08:44:15 +0200

    prettier

--- examples/express-sample/routes/index.js
+++ examples/express-sample/routes/index.js
@@ -10,27 +10,29 @@ router.get("/", function (req, res, next) {
 });
 
 router.get("/send", function (req, res, next) {
-
   raygunClient.addBreadcrumb({
     level: "debug",
     category: "Example",
     message: "Breadcrumb in /send endpoint",
     customData: {
       "custom-data": "data",
-    }
+    },
   });
 
-  raygunClient.send("Error in /send endpoint").then((message) => {
-    res.render("send", {
-      title: "Sent custom error to Raygun",
-      body: `Raygun status code: ${message.statusCode}`,
+  raygunClient
+    .send("Error in /send endpoint")
+    .then((message) => {
+      res.render("send", {
+        title: "Sent custom error to Raygun",
+        body: `Raygun status code: ${message.statusCode}`,
+      });
+    })
+    .catch((error) => {
+      res.render("send", {
+        title: "Failed to send custom error to Raygun",
+        body: error.toString(),
+      });
     });
-  }).catch((error) => {
-    res.render("send", {
-      title: "Failed to send custom error to Raygun",
-      body: error.toString(),
-    });
-  })
 });
 
 router.get("/error", function (req, res, next) {
@@ -40,7 +42,7 @@ router.get("/error", function (req, res, next) {
     message: "Breadcrumb in /error endpoint",
     customData: {
       "custom-data": "data",
-    }
+    },
   });
 
   // Call an object that doesn't exist to send an error to Raygun
