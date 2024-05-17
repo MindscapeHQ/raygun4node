41bacd1cf21e4faf6fe54f565c722279078e3d0b
Miguel Beltran <m@beltran.work>
2024-05-17 08:29:54 +0200

    uncomment mistake

--- examples/express-sample/raygun.client.js
+++ examples/express-sample/raygun.client.js
@@ -1,11 +1,11 @@
 var config = require("config");
 
-// if (config.Raygun.Key === "YOUR_API_KEY") {
-//   console.error(
-//     "[Raygun4Node-Express-Sample] You need to set your Raygun API key in the config file",
-//   );
-//   process.exit(1);
-// }
+if (config.Raygun.Key === "YOUR_API_KEY") {
+  console.error(
+    "[Raygun4Node-Express-Sample] You need to set your Raygun API key in the config file",
+  );
+  process.exit(1);
+}
 
 // Setup Raygun
 var raygun = require("raygun");
