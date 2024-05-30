707f5ec5b42a8a1d60fa35466d36adfe627fb43b
Miguel Beltran <m@beltran.work>
2024-05-30 12:04:22 +0200

    created aws example

---
+++ examples/aws-lambda-sample/index.js
@@ -0,0 +1,8 @@
+const raygun = require("raygun");
+const {awsHandler} = require("@raygun/aws-lambda");
+
+const client = new raygun.Client().init({ apiKey: process.env.RAYGUN });
+
+exports.handler = awsHandler({ client }, async function (event, context) {
+    throw "It's an AWS error!";
+});
