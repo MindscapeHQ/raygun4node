301033cc35ed90e61a00750b73516054bfec157b
Miguel Beltran <m@beltran.work>
2024-05-31 10:21:28 +0200

    create callback example

--- examples/aws-lambda-sample/index.js
+++ examples/aws-lambda-sample/index.js
@@ -1,9 +1,25 @@
 const raygun = require("raygun");
 const { awsHandler } = require("@raygun/aws-lambda");
+const {call} = require("express");
 
 const client = new raygun.Client().init({ apiKey: process.env.RAYGUN });
 
 exports.handler = awsHandler({ client }, async function (event, context) {
-  client.addBreadcrumb("breadcrumb before error");
-  throw "It's an AWS error!";
+  client.addBreadcrumb("breadcrumb on event received");
+  if (event["error"]) {
+    client.addBreadcrumb("event has error data!");
+    throw "It's an AWS error!";
+  } else {
+    return "all good!";
+  }
 });
+
+// exports.handler = awsHandler({ client }, function (event, context, callback) {
+//   client.addBreadcrumb("breadcrumb on event received");
+//   if (event["error"]) {
+//     client.addBreadcrumb("event has error data!");
+//     callback("AWS Error from callback!");
+//   } else {
+//     callback(null, "all good!");
+//   }
+// });
