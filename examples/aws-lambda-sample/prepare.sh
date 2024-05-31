c2d03a13646c91cdb0087d15cf715aea4acea957
Miguel Beltran <m@beltran.work>
2024-05-31 08:22:57 +0200

    fixing example

---
+++ examples/aws-lambda-sample/prepare.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+echo "Delete old zip file..."
+rm -f example.zip
+
+# Build app with option "install-links" so no symbolic-links are created
+echo "Building the node app..."
+npm ci --install-links
+
+# Zip all the contents of the folder (excluding this script)
+echo "Zipping all the contents of the folder..."
+zip -r example.zip . -x "prepare.sh"
+
+echo "Done!"
