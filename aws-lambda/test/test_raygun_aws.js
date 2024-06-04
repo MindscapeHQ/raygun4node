46554e58df7dd83a512ea91cae3459f47eed35a9
Miguel Beltran <m@beltran.work>
2024-06-04 10:34:39 +0200

    add more tests

--- aws-lambda/test/test_raygun_aws.js
+++ aws-lambda/test/test_raygun_aws.js
@@ -28,3 +28,60 @@ test("capture AWS lambda errors", async function (t) {
 
   t.equal(message.details.error.message, "error");
 });
+
+test("pass result to AWS Lambda", async function (t) {
+  t.plan(1);
+  // Setup client
+  const testEnvironment = await makeClientWithMockServer();
+  const client = testEnvironment.client;
+
+  // Setup lambda that crashes
+  const lambda = awsHandler({ client }, async (event, context) => {
+    return { result: true };
+  });
+
+  try {
+    // Call to lambda
+    const response = await lambda({ event: "event" }, { functionName: "test" });
+    t.equal(response.result, true);
+  } catch (e) {
+    t.fail();
+  }
+
+  testEnvironment.stop();
+});
+
+test("include scoped breadcrumbs", async function (t) {
+  // Setup client
+  const testEnvironment = await makeClientWithMockServer();
+  const client = testEnvironment.client;
+
+  // Setup lambda that crashes
+  const lambda = awsHandler({ client }, async (event, context) => {
+    // Add a custom breadcrumb
+    client.addBreadcrumb("custom breadcrumb");
+    // Fail
+    throw "error";
+  });
+
+  const nextRequest = testEnvironment.nextRequest();
+
+  try {
+    // Call to lambda
+    await lambda({ event: "event" }, { functionName: "test" });
+  } catch (e) {
+    // error should be re-thrown to AWS
+    t.equal(e, "error");
+  }
+
+  const message = await nextRequest;
+
+  testEnvironment.stop();
+
+  // Includes both internal and custom breadcrumbs
+  t.equal(message.details.breadcrumbs.length, 2);
+  // internal breadcrumb
+  t.equal(message.details.breadcrumbs[0].message, "Running AWS Function: test");
+  // custom breadcrumb
+  t.equal(message.details.breadcrumbs[1].message, "custom breadcrumb");
+});
