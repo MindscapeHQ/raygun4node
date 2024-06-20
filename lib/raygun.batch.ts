1b82098d862a1ac2a7714ba33bc368d29daeb4f9
Miguel Beltran <m@beltran.work>
2024-06-20 10:46:18 +0200

    create promise reference

--- lib/raygun.batch.ts
+++ lib/raygun.batch.ts
@@ -5,22 +5,27 @@ import { HTTPOptions, SendOptions } from "./types";
 
 const debug = require("debug")("raygun");
 
-export type MessageAndCallback = {
+export type MessageAndPromise = {
   serializedMessage: string;
-  resolve: (message: IncomingMessage) => void;
-  reject: (error: Error) => void;
+  promise: PromiseReference<IncomingMessage>;
 };
 
 export type PreparedBatch = {
   payload: string;
   messageCount: number;
-  resolves: Array<(message: IncomingMessage) => void>;
-  rejects: Array<(error: Error) => void>;
+  promises: Array<PromiseReference<IncomingMessage>>;
 };
 
 export type BatchState = {
-  messages: MessageAndCallback[];
+  messages: MessageAndPromise[];
   messageSizeInBytes: number;
+};
+
+// Contains the resolve and reject methods of the original promise
+// Allows to call to resolve and reject from outside the Promise scope
+type PromiseReference<T> = {
+  resolve: (message: T) => void;
+  reject: (error: Error) => void;
 };
 
 export const MAX_MESSAGES_IN_BATCH = 100;
@@ -96,7 +101,8 @@ export class RaygunBatchTransport {
     }
 
     const promise = new Promise<IncomingMessage>((resolve, reject) => {
-      this.batchState.messages.push({ serializedMessage, resolve, reject });
+      const promise = { resolve, reject };
+      this.batchState.messages.push({ serializedMessage, promise });
     });
 
     const batchIsFull = this.batchState.messages.length === 100;
@@ -116,8 +122,7 @@ export class RaygunBatchTransport {
     const batch: PreparedBatch = {
       payload: `[${payload}]`,
       messageCount: this.batchState.messages.length,
-      resolves: this.batchState.messages.map((m) => m.resolve),
-      rejects: this.batchState.messages.map((m) => m.reject),
+      promises: this.batchState.messages.map((m) => m.promise),
     };
 
     this.sendBatch(batch);
@@ -128,7 +133,7 @@ export class RaygunBatchTransport {
   }
 
   private sendBatch(batch: PreparedBatch) {
-    const { payload, messageCount, resolves, rejects } = batch;
+    const { payload, messageCount, promises } = batch;
 
     debug(
       `[raygun.batch.ts] Batch transport - processing (${messageCount} message(s) in batch)`,
@@ -138,7 +143,7 @@ export class RaygunBatchTransport {
 
     this.batchId++;
 
-    const runAllCallbacks = (
+    const resolvePromises = (
       err: Error | null,
       response: IncomingMessage | null,
     ) => {
@@ -147,17 +152,15 @@ export class RaygunBatchTransport {
         debug(
           `[raygun.batch.ts] Batch transport - error sending batch (id=${batchId}, duration=${durationInMs}ms): ${err}`,
         );
-        for (const reject of rejects) {
-          reject(err);
+        for (const promise of promises) {
+          promise.reject(err);
         }
       } else {
         debug(
           `[raygun.batch.ts] Batch transport - successfully sent batch (id=${batchId}, duration=${durationInMs}ms)`,
         );
-        if (response) {
-          for (const resolve of resolves) {
-            resolve(response);
-          }
+        for (const promise of promises) {
+          promise.resolve(response!);
         }
       }
     };
@@ -173,12 +176,10 @@ export class RaygunBatchTransport {
       http: this.httpOptions,
     })
       .then((response) => {
-        // Call to original callbacks for success
-        runAllCallbacks(null, response);
+        resolvePromises(null, response);
       })
       .catch((error) => {
-        // Call to original callbacks for error
-        runAllCallbacks(error, null);
+        resolvePromises(error, null);
       });
   }
 }
