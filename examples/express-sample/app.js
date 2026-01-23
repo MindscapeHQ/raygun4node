ba6b2ac3c340f734250b16cf4d2462fcab1da2ec
Kai Koenig <kai@ventego-creative.co.nz>
2026-01-24 00:11:38 +1300

    fix: broken express-sample and dependency updates in domains sample (#448)
    * Express sample had issues due to outdated (and not needed) deependencies.
    
    * Minor dependency updates in domains sample

--- examples/express-sample/app.js
+++ examples/express-sample/app.js
@@ -4,7 +4,7 @@ var favicon = require("serve-favicon");
 var logger = require("morgan");
 var cookieParser = require("cookie-parser");
 var bodyParser = require("body-parser");
-var sassMiddleware = require("node-sass-middleware");
+
 var raygunClient = require("./raygun.client");
 
 var routes = require("./routes/index");
@@ -33,15 +33,6 @@ app.use(
   }),
 );
 app.use(cookieParser());
-app.use(
-  sassMiddleware({
-    src: __dirname,
-    dest: path.join(__dirname, "public"),
-    debug: true,
-    outputStyle: "compressed",
-    prefix: "/stylesheets",
-  }),
-);
 app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/", routes);
